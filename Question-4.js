b12af6232f4d780a3def8a821c49a24d00569551
Ishasal <iisha3627@gmail.com>
2022-09-19 10:52:52 -0400

    Answers to the following questions

--- Question-4.js
+++ Question-4.js
@@ -1,7 +1,14 @@
 
 function countToTen(){
     //Use a while loop to count to ten, using console.log to print what number you are on
-    // (YOUR CODE HERE)
+   
+let x = 0;
+while( x <= 10 ){
+    console.log("Counting",x);
+    x++;
 }
+
+}
+
 
 countToTen();
--- Question-5.js
+++ Question-5.js
@@ -1,9 +1,21 @@
 
 function countToN(n){
     //Use a while loop to count to ten, using console.log to print what number you are on
-    // (YOUR CODE HERE)
-}
-
+    
+    let x = 0;
+    while( x <= n ){
+        console.log("Counting to N",x);
+        x++;
+    }
+    }
 countToN(3);
 
+for (let x = 0; x <= 3; x++ ) {
+    console.log("Counting to N", x);
+}
+
 countToN(5);
+
+for (let x = 0; x <= 5; x++ ) {
+    console.log("Counting to N", x);
+}
